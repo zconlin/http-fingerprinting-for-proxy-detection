ced16661b209b0094a2601896abdb5b9f1762511
Zac Conlin <zac.conlin@gmail.com>
2025-02-19 12:17:58 -0700

    Updated for production with WSGI/Gunicorn

--- yara/app.py
+++ yara/app.py
@@ -35,6 +35,3 @@ def process_log():
         return jsonify({"proxy_detected": True, "details": detected})
     else:
         return jsonify({"proxy_detected": False, "details": []})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
